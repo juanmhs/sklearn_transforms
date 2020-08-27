3016edd297400b2b15e4cf638ed2917ddfc7ec22
Juan Huidobro <juanmhs15@gmail.com>
2020-08-27 12:22:40 -0400

    add transform oversampling

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,27 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+    
+class OverSampling(BaseEstimator, TransformerMixin):
+    def __init__(self):
+       pass
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Primero copiamos el dataframe de datos de entrada 'X'
+        data = X.copy()
+        
+        count_class_0, count_class_1 = data.OBJETIVO.value_counts()
+
+        # Divide by class
+        df_class_0 = data[data['OBJETIVO'] == "Aceptado"]
+        df_class_1 = data[data['OBJETIVO'] == "Sospechoso"]
+
+        #OVER
+        df_class_1_over = df_class_1.sample(count_class_0, replace=True)
+        df_test_over = pd.concat([df_class_0, df_class_1_over], axis=0)
+
+        return df_test_over
